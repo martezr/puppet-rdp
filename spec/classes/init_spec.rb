6b1e26c189bfe41fe2d323cc8356adcf94b8387f
martezr <martez.reed@greenreedtech.com>
2016-06-17 10:30:34 -0500

    Correct errors in rspec tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -3,14 +3,17 @@ describe 'rdp', :type => :class do
   context 'with the default parameters' do
     it { should compile.with_all_deps }
     it { should contain_class('rdp')}
+  end
+
+  context 'with default parameters on windows' do
+    let(:facts) { { :kernel => 'windows' } }
     it { should contain_registry_value('rdp')}
     it { should contain_registry_value('rdp_nla')}
     it { should contain_windows_firewall_exception('rdp_firewall')}
   end
-
+  
   context 'On a non-windows OS' do
     let(:facts) { { :kernel => 'Linux' } }
-
     it { expect { should raise_error(Puppet::Error) }}
   end
 end
